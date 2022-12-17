a6951edbd79f18aaebdd2dd7365c836d63e436d4
pbalykov <p.d.balykov@gmail.com>
2022-12-17 22:41:19 +0300

    "add design and operation of some buttons"

--- headers/root.hpp
+++ headers/root.hpp
@@ -16,12 +16,31 @@ public:
     ~root();
 
 private slots:
+    void on_button_game_clicked();
+    void on_button_customization_clicked();
+    void on_button_abaut_game_clicked();
     void on_button_exit_clicked();
 
+    void on_game_back_clicked();
+    void on_game_exit_clicked();
+
+    void on_about_game_back_clicked();
+    void on_about_game_exit_clicked();
+
 private:
+    enum class WINDOW_TYPES{
+        MENU = 0,
+        GAME = 1,
+        CUSTOMIZATION = 2,
+        ABOUT_THE_GAME = 3,
+    };
+    void _switching_window(WINDOW_TYPES other);
+
     Ui::root* _ui;
     Snake* _game;
     QTimer* _timer;
+    WINDOW_TYPES _parent_window;
+    WINDOW_TYPES _current_window;
 };
 
 #endif
--- scr/root.cpp
+++ scr/root.cpp
@@ -2,12 +2,22 @@
 #include "ui_root.h"
 #include <QTabBar>
 
+void root::_switching_window(WINDOW_TYPES other) {
+    this->_parent_window = this->_current_window;
+    this->_current_window = other;
+    int index_window = static_cast<int>(other);
+    this->_ui->tab_game->setCurrentIndex(index_window);
+    return ;
+}
+
 root::root(QWidget* parent) : QMainWindow(parent),
-    _ui(new Ui::root), _game(nullptr), _timer(nullptr) {
+    _ui(new Ui::root), _game(nullptr), _timer(nullptr),
+    _current_window(WINDOW_TYPES::MENU) {
 
     this->_ui->setupUi(this);
     this->_ui->tab_game->tabBar()->hide();
-   // this->_ui->tab_game->setCurrentIndex(0);
+    this->_switching_window(WINDOW_TYPES::MENU);
+    return ;
 }
 
 root::~root() {
@@ -17,8 +27,41 @@ root::~root() {
     return ;
 }
 
+void root::on_button_game_clicked() {
+    this->_switching_window(WINDOW_TYPES::GAME);
+    return ;
+}
+
+void root::on_button_customization_clicked() {
+    this->_switching_window(WINDOW_TYPES::CUSTOMIZATION);
+    return ;
+}
+
+void root::on_button_abaut_game_clicked() {
+    this->_switching_window(WINDOW_TYPES::ABOUT_THE_GAME);
+    return ;
+}
 
 void root::on_button_exit_clicked() {
+    this->close();
+    return ;
+}
+
+void root::on_game_back_clicked() {
+    this->_switching_window(WINDOW_TYPES::MENU);
+    return ;
+}
+
+void root::on_game_exit_clicked() {
+    this->close();
+    return ;
+}
+
+void root::on_about_game_back_clicked() {
+    this->_switching_window(WINDOW_TYPES::MENU);
+    return ;
+}
+void root::on_about_game_exit_clicked() {
     this->close();
     return ;
 }
